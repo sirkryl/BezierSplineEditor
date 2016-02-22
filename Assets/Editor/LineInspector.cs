2d93072d9e39f5e288ace2314102a0fe3eb3151f
Chrissly <chrisvhuber@gmail.com>
2016-02-22 18:19:29 +0100

    + initial commit

---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor {
+
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+        Transform handleTransform = line.transform;
+        Vector3 p0 = handleTransform.TransformPoint(line.p0);
+        Vector3 p1 = handleTransform.TransformPoint(line.p1);
+        Quaternion handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+
+        Handles.color = Color.white;
+        Handles.DrawLine(p0, p1);
+
+        EditorGUI.BeginChangeCheck();
+        p0 = Handles.DoPositionHandle(p0, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p0 = handleTransform.InverseTransformPoint(p0);
+        }
+        EditorGUI.BeginChangeCheck();
+        p1 = Handles.DoPositionHandle(p1, handleRotation);
+        if (EditorGUI.EndChangeCheck()) 
+        {
+            Undo.RecordObject(line, "Move Point");
+            EditorUtility.SetDirty(line);
+            line.p1 = handleTransform.InverseTransformPoint(p1);
+        }
+
+        
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
